b0ace5b0cedf50c834cdfa7e3f077e67034a7ca2
milpen <milpen@mail.ru>
2023-09-18 21:03:22 +0500

    task 2 variant 2

--- week15/week15/Task 2 cities/index.js
+++ week15/week15/Task 2 cities/index.js
@@ -1,7 +1,7 @@
 const citiesList = document.getElementById('cities_list'); //получаем элемент из html
 
 const cities = ["Москва", "Санкт-Петербург", "Токио", "Нью-Йорк"]; // создаю массив 4 городов
-let citiesTemperature = [5, 12, -8, 4]; // создаю пустой массив для хранения значений температуры
+let citiesTemperature = []; // создаю пустой массив для хранения значений температуры
 for(let i = 0; i< cities.length; i++) {
 const cityInput = prompt(`Введите температуру для ${cities[i]}`);//запрашиваем у пользователя температуру
 citiesTemperature.push(Number(cityInput)); // добавляем данные в массив, переводя их в число
@@ -21,5 +21,41 @@ minTempShown.textContent = `Минимальная температура: ${min
 citiesList.after(minTempShown);//добавляю текст после списка городов
 
 const maxTempShown = document.createElement('h4');//создаю элемент для вывода макс.температуры
-maxTempShown.textContent = `Максимальная температура: ${maxTemp} градусов Цельсия`;//выводу текст
+maxTempShown.textContent = `Максимальная температура: ${maxTemp} градусов Цельсия`;//вывожу текст
 citiesList.after(maxTempShown);//добавляю текст после списка городов
+
+//Вариант 2
+/*const citiesList = document.getElementById('cities_list');
+
+const cities = ["Москва", "Санкт-Петербург", "Токио", "Нью-Йорк"];
+let citiesTemperature = [];
+
+for(let i = 0; i< cities.length; i++) {
+const cityInput = prompt(`Введите температуру для ${cities[i]}`);
+citiesTemperature.push(Number(cityInput));
+
+const cityElem = document.createElement('li');
+cityElem.textContent = `Температура в городе ${cities[i]}: ${cityInput} градусов Цельсия`;
+citiesList.append(cityElem);
+};
+//Создаю две переменные, где значения гарантированно больше или меньше возможных температур (поняла пункт 6 задания так)
+let minimum = Infinity;
+let maximum = -Infinity;
+
+for (let i=0; i<citiesTemperature.length; i++) {
+    const temp = citiesTemperature[i];
+    if (temp<minimum){
+        minimum=temp;
+    }
+    if (temp>maximum){
+        maximum=temp;
+    }
+}
+
+const minTempShown = document.createElement('h4');
+minTempShown.textContent = `Минимальная температура: ${minimum} градусов Цельсия`;
+citiesList.after(minTempShown);
+
+const maxTempShown = document.createElement('h4');
+maxTempShown.textContent = `Максимальная температура: ${maximum} градусов Цельсия`;
+citiesList.after(maxTempShown);*/
